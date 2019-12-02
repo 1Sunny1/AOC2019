200acb9f586c11c0495f93469315ea7567d87eb9
1Sunny1 <38404136+1Sunny1@users.noreply.github.com>
2019-12-02 17:01:24 +0100

    Update Source.cpp
    day one

--- day one/Source.cpp
+++ day one/Source.cpp
@@ -4,19 +4,10 @@
 #include <vector>
 #include <algorithm>
 #include <iterator>
-#include <iostream>
-
-//auto fuelNeeded() {
-//	std::fstream f("input.txt");
-//	std::vector<int> vals;
-//	std::copy(std::istream_iterator<int>(f), std::istream_iterator<int>(), std::back_inserter(vals));
-//	return std::accumulate(std::begin(vals), std::end(vals), 0, [](auto sum, auto value) { return sum += std::round(value / 3) - 2; });
-//}
 
 auto main() -> int {
 	std::fstream f("input.txt");
 	std::vector<int> vals;
 	std::copy(std::istream_iterator<int>(f), std::istream_iterator<int>(), std::back_inserter(vals));
 	return std::accumulate(std::begin(vals), std::end(vals), 0, [](auto sum, auto value) { return sum += std::round(value / 3) - 2; });
-	system("pause");
 }
